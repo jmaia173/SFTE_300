128588a3c5291c930980e527b655d636c76f5ffd
jmaia173 <jpmascarenhasmaia@gmail.com>
2025-09-04 08:37:17 -0700

    Initial commit

---
+++ MERN/backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/products", (req, res) => {
+    res.send("Server is ready123");
+});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5500, () => {
+    connectDB();    
+    console.log('Server started at http://localhost:5500');
+})
---
+++ MERN/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+import const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect()
+    }
+}
